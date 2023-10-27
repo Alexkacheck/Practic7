292e2f818f85cb094a3d90b7d13eb7157c3e8b50
Alexander Chechetko <ggg.kacheck@gmail.com>
2023-10-27 11:37:58 +0600

    Добавьте файлы проекта.

---
+++ Practic7/ArrayContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic7
+{
+    class ArrayContainer
+    {
+        private int[] array;
+
+        public ArrayContainer(int[] arr)
+        {
+            array = arr;
+        }
+
+        public static bool operator >(ArrayContainer obj1, ArrayContainer obj2)
+        {
+            return obj1.GetSum() > obj2.GetSum();
+        }
+
+        public static bool operator <(ArrayContainer obj1, ArrayContainer obj2)
+        {
+            return obj1.GetSum() < obj2.GetSum();
+        }
+
+        private int GetSum()
+        {
+            int sum = 0;
+            foreach (int element in array)
+            {
+                sum += element;
+            }
+            return sum;
+        }
+    }
+
+ 
+}
---
+++ Practic7/Equals.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic7
+{
+    class MyClass
+    {
+        public int Value { get; set; }
+
+        public MyClass(int value)
+        {
+            Value = value;
+        }
+
+        public static bool operator ==(MyClass obj1, MyClass obj2)
+        {
+            return obj1.Value == obj2.Value;
+        }
+
+        public static bool operator !=(MyClass obj1, MyClass obj2)
+        {
+            return obj1.Value != obj2.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is MyClass other)
+            {
+                return this.Value == other.Value;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass obj1 = new MyClass(5);
+            MyClass obj2 = new MyClass(10);
+
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("Объекты равны.");
+            }
+            else
+            {
+                Console.WriteLine("Объекты не равны.");
+            }
+        }
+    }
+}
